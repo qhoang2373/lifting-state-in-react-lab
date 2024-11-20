f72717fa94f7227baba9232802a937418e9951c5
Quan <qhoang2373@gmail.com>
2024-11-19 19:04:59 -0800

    add .map for ingredient list and button

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -4,16 +4,11 @@ import '../App.css';
 const IngredientList = (props) => {
     return <ul> 
         {props.inStockIngredients.map((inStockIngredient, index) => {
-           return (
-            <li key={index} 
-            style={{ backgroundColor: ingredient.color }}>
-              {ingredient.name}
-              
-              <button onClick={() => addToBurger(ingredient)}>+</button>
-            </li>
-          );
-        })}
-      </ul>
+          
+          const
+           
+           <button onClick={() => addToBurger(ingredient)}>+</button>
+    </ul>
   };
   
   export default IngredientList;
