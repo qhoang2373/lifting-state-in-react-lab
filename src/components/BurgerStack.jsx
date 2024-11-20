8358ffd6f551085095285af45a52f24dd55321fe
Quan <qhoang2373@gmail.com>
2024-11-19 19:04:18 -0800

    create return statement

--- src/components/BurgerStack.jsx
+++ src/components/BurgerStack.jsx
@@ -4,13 +4,15 @@ const BurgerStack = (props) => {
     return (
         <ul>
           {stack.map((ingredient, index) => {
-            
-         }
-
-
+        return (
+            <li key={index} style={{backgroundColor: ingredient.color}}>
+            {ingredient.name}
+            <button onClick={() => removeFromBurger(ingredient)}>-</button>
+        </li>    
+     )
+    })}
     </ul>
-);
-
-  
+    )
+    };
   export default BurgerStack;
   
