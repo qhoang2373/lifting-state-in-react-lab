7ae41c8ae8135495e15ef7802d0d03de7c263aad
Quan <qhoang2373@gmail.com>
2024-11-19 19:05:38 -0800

    create return statement

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -4,11 +4,15 @@ import '../App.css';
 const IngredientList = (props) => {
     return <ul> 
         {props.inStockIngredients.map((inStockIngredient, index) => {
-          
-          const
-           
+        return (
+            <li key={index} 
+            style={{ backgroundColor: ingredient.color }}>
+              {ingredient.name}
            <button onClick={() => addToBurger(ingredient)}>+</button>
-    </ul>
+           </li>
+          );
+        })}
+      </ul>
   };
   
   export default IngredientList;
